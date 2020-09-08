00790261a8de081961bd0af84c4baf60d4b75253
Joohyuck <joohyuck.seo@gmail.com>
2020-09-09 00:46:43 +0900

    Add 10172 problem

---
+++ 1. 입출력과 사칙연산/10172.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+
+	return 0;
+}
