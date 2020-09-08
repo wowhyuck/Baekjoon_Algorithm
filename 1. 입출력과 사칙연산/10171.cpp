09d8aa7c3713f4794130fd151969340cd77611c0
Joohyuck <joohyuck.seo@gmail.com>
2020-09-09 00:19:40 +0900

    Add 10171 problem

---
+++ 1. 입출력과 사칙연산/10171.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+
+	return 0;
+}
