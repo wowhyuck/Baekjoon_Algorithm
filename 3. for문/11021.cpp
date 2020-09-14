7d9609ef4c56920ff54c9bbc3431b0062873aad5
Joohyuck <joohyuck.seo@gmail.com>
2020-09-14 12:56:52 +0900

    Add 11021 problem

---
+++ 3. for문/11021.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int a, b, size;
+	cin >> size;
+
+	for (int i = 1; i <= size; i++)
+	{
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a + b << "\n";
+	}
+
+	return 0;
+}
