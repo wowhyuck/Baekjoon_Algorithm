68a35225b5faed84c3368fcd9253e742744afc8c
Joohyuck <joohyuck.seo@gmail.com>
2020-09-14 12:57:09 +0900

    Add 2741 problem

---
+++ 3. for문/2741.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int num;
+	cin >> num;
+
+	for (int i = 1; i <= num; i++)
+	{
+		cout << i << "\n";
+	}
+
+	return 0;
+}
